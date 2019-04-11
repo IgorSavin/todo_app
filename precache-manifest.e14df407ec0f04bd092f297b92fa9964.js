5c5a28a7ee3fedb3d5e9648301d22c9a47ef761d
Igor Savin <beixiaofeng@gmail.com>
2019-04-11 23:49:52 +0200

    Updates

---
+++ precache-manifest.e14df407ec0f04bd092f297b92fa9964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c1ee6e246643c8a5a1d",
+    "url": "/todo_app/static/js/runtime~main.da743e9d.js"
+  },
+  {
+    "revision": "d662e719b041f52f6890",
+    "url": "/todo_app/static/js/main.5c7a73f6.chunk.js"
+  },
+  {
+    "revision": "45f2e7d1c032b406779e",
+    "url": "/todo_app/static/js/2.991b768e.chunk.js"
+  },
+  {
+    "revision": "d662e719b041f52f6890",
+    "url": "/todo_app/static/css/main.2a2d02bf.chunk.css"
+  },
+  {
+    "revision": "c8bdc341f3c544717032315d22f2706b",
+    "url": "/todo_app/index.html"
+  }
+];
